7411059fc1883cfb2f9adf15a71e6f6eeb3b4551
IfeanYiGodsgift <godsgift.ifeanyi@pau.edu.ng>
2025-02-05 09:49:45 +0100

    pushed

---
+++ week5/arrayAndPointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main (){
+    char array[5] = {
+        'a','s','h','e','n'
+    };
+
+    int rowMatrix[3][2]={
+        {1,2},
+        {3,4},
+        {5,10}
+    };
+
+    printf("%s",array);
+    printf("%i",matrix[1][1]);
+    return 0;
+}
